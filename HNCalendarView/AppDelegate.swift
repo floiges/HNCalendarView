323c1f65fc7b187ccd428cd3cb8aa2746381864e
floiges <floiges@163.com>
2017-06-14 12:55:27 +0800

    init project

---
+++ HNCalendarView/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HNCalendarView
+//
+//  Created by 刘亚东 on 2017/6/14.
+//  Copyright © 2017年 floiges. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HNCalendarView/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HNCalendarView
+//
+//  Created by 刘亚东 on 2017/6/14.
+//  Copyright © 2017年 floiges. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
